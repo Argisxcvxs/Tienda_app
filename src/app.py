38add30e2db94bbb201372f6f34ac5af0fd379d4
Claudio Augusto Chacon Tavera <augustochacon2007@gmail.com>
2025-01-09 23:33:26 -0500

    empezamos a acomodar donde poner backend y frontend

--- src/app.py
+++ src/app.py
@@ -1,6 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from config.config import Config
+from models.db import db
+   
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -9,10 +11,13 @@ db = SQLAlchemy(app)
 # Importar los modelos para que SQLAlchemy los reconozca
 from models.db import Cliente, Proveedor, Producto, Venta, DetalleVenta, Factura, Usuario
 
+"codigo"
+
 # Definir una ruta de ejemplo
 @app.route('/')
 def index():
-    return '¡Hola, mundo!'
+    "front end"
+    return render_template('index.html')
 
 # Ejecutar la aplicación
 if __name__ == '__main__':
